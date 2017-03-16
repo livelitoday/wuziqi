46af0050661f5a3b9fafd6ff1a4b1fb0638b0ea1
yiyanggui <1418749937@qq.com>
2017-03-16 20:18:40 +0800

    修改错误

--- app/src/main/java/utils/MyApplication.java
+++ app/src/main/java/utils/MyApplication.java
@@ -7,7 +7,7 @@ import android.content.Context;
  * 项目名称：com.test.wuziqi
  * 创建人：Created by zhiyuan.
  * 创建时间：Created on 2016/8/31 19:05
- * 修改备注：让程序启动的时候初始化MyApplication，而不是默认的Application，借助此工具类可以在
+ * 修改备注：让程序启动的时候初始化MyApplication，而不是默认的Application，借助此工具类可以在全局范围内获取Application级别的Context.
  */
 public class MyApplication extends Application {
     private static Context context;
